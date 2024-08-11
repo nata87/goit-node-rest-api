269730cda78a4640459b7496e3d2dc51dbaec3b0
Nataliya Pustelnyk <nata_pustelnyk@ukr.net>
2024-08-11 13:47:05 +0200

    fix

--- db/models/Contact.js
+++ db/models/Contact.js
@@ -1,7 +1,7 @@
 import sequelize from "../sequelize.js";
 import { DataTypes } from "sequelize";
 
-const Contact = sequelize.define("Contacts", {
+const Contact = sequelize.define("contacts", {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
