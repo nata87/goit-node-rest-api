10ca0e809b9c855393ed5380b8b636303c00c1b3
Nataliya Pustelnyk <nata_pustelnyk@ukr.net>
2024-08-11 23:10:25 +0200

    fix

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,6 +1,27 @@
 import { Sequelize } from "sequelize";
-import { databaseConfig } from "./config.js";
+// import { databaseConfig } from "./config.js";
 
-const sequelize = new Sequelize(databaseConfig);
+// const sequelize = new Sequelize(databaseConfig);
+
+const {
+  DATABASE_DIALECT,
+  DATABASE_NAME,
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+} = process.env;
+
+const sequelize = new Sequelize({
+  dialect: DATABASE_DIALECT,
+  database: DATABASE_NAME,
+  host: DATABASE_HOST,
+  port: DATABASE_PORT,
+  username: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  dialectOptions: {
+    ssl: true,
+  },
+});
 
 export default sequelize;
